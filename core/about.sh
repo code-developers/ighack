c17ac9c2ca9b94fabaebee6ba1f20805bcedb843
krishpranav <krisna.pranav@gmail.com>
2021-04-13 13:45:34 +0530

    about.sh updated

---
+++ core/about.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+clear
+echo 'HELLO' | lolcat
+
+echo " ABOUT US " | lolcat
+echo " Hey There We Are Code-Developers We Make Tools For Cyber Security Purpose" | lolcat
+
+sleep 8.0
+cd $HOME/ighack
+bash ighack.sh
